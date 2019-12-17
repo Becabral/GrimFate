dfe198f6fd9b2ae61d75ec42f5a38458312349e7
Miguel Duarte <miguelfelicianod@gmail.com>
2019-12-17 22:28:35 +0000

    TESTING FMOD
    é capaz de correr mal, vamos ver

---
+++ scripts/fmod_start.gd
@@ -0,0 +1,26 @@
+extends Node
+
+func _ready():
+	# initialize FMOD
+	# initializing with the LIVE_UPDATE flag lets you
+	# connect to Godot from the FMOD Studio editor
+	# and author events in realtime
+	Fmod.system_init(1024, Fmod.FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.FMOD_INIT_VOL0_BECOMES_VIRTUAL)
+
+	# load banks
+	# place your banks inside the project directory
+	Fmod.bank_load("./assets/sound/Desktop/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.bank_load("./assets/sound/Desktop/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.bank_load("./assets/sound/Desktop/Music.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+
+	# register a listener
+	Fmod.system_add_listener($Listener)
+
+	# play some events
+	Fmod.play_one_shot("event:/Music", $SoundSource1)
+
+func _process(delta):
+	# update FMOD every tick
+	# calling system_update also updates the listener 3D position
+	# and 3D positions of any attached event instances
+	Fmod.system_update()
