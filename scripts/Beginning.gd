bf8975bb4875c550aad49102617c7cb8e907fda5
Becabral <Bebrasilcabral@gmail.com>
2019-12-19 23:40:58 +0000

    Correção do modo de coleta dos itens
    Agora toda a coleta é gerida pela cena principal. Dessa forma é mais fácil de enviar sinais para as outras cenas como (Interface, Player e o objeto coletado)

--- scripts/Beginning.gd
+++ scripts/Beginning.gd
@@ -1,13 +1,28 @@
 extends Node2D
+onready var lamp = preload("res://scenes/Lamp.tscn")
+var lampada
 
-
+	
 func _ready():
 	# inicializa o personagem com o estado e a direção pra onde olha
 	$Player.STATE = "sideView"
 	$Player.spritedir = "right"
+	create_lamp()
+	
 	
 func _process(delta):
-	pass
+	if $Player.lamp == false: # verificador para evitar erro ao testar a lampada já excluída
+		if lampada.touch == true and Input.is_action_just_pressed("use"):
+			$Player.lamp = true # <- usado para mudar o sprite com a lanterna na mao
+			#gui.lamp = true <- implementar a passagem da variavel para a interface
+			lampada.queue_free()
+	
 
-
-
+func create_lamp():
+	lampada = lamp.instance()
+	lampada.position.x = 112.21
+	lampada.position.y = 545.314
+	lampada.scale.x = 0.2
+	lampada.scale.y = 0.2
+	add_child(lampada)
+ 
--- scripts/Lamp.gd
+++ scripts/Lamp.gd
@@ -1,21 +1,13 @@
 extends Area2D
 
 var touch = false
-var coletado = false
 
 func _ready():
 	$useLbl.hide()
 	pass 
 
-
 func _process(_delta):
-	if coletado == true:
-		#!!!! como fazer para deletar esse item ao inves de esconder o sprite
-		#!!!! assim :P
-		queue_free()
-	else:
-		if touch == true:
-			coleta()
+	pass
 
 func _on_Lamp_body_entered(body):
 	$useLbl.show()
@@ -23,10 +15,4 @@ func _on_Lamp_body_entered(body):
 
 func _on_Lamp_body_exited(body):
 	$useLbl.hide()
-
-func coleta():
-	if Input.is_action_just_pressed("use"):
-		#touch = false
-		coletado = true
-		get_node("../Player").lamp = true
 
