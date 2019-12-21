1815acd068505f378a352b99a2d5d05c582807d8
Miguel Duarte <miguelfelicianod@gmail.com>
2019-12-21 14:31:17 +0000

    Added GrimEnd scene (faceoff with Death)
    Added a camera animation to the Player scene.
    
    Made use of it in the GrimEnd scene

---
+++ scripts/GrimEnd.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _on_CutsceneTrigger_body_entered(body):
+	if body.get_name()=="Player":
+		$Player.set_physics_process(false)
+		$Player.anim_switch("idle")
+		$Player/Camera2D/AnimPlayerCamera.play("CameraPan")
+	pass # Replace with function body.
