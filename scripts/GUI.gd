fd8f92a2e67eef4700b7bb74a2fdef19a59d7084
Becabral <Bebrasilcabral@gmail.com>
2020-01-09 17:39:31 +0000

    Atualização da estrutura do GUI com a adição da Skill
    Adicionado a area das skills (Dash e Scythe)
    
    - Falta modificar os icones e programar a sua interação

--- scripts/GUI.gd
+++ scripts/GUI.gd
@@ -4,16 +4,25 @@ var countSeed = 0
 var seedDisplay=-1
 var player
 
+#cor dah - bf3760
+#cor scythe - 4ff057
+
 func _ready():
-	$MarginContainer/Items/ItemLbl.visible = false
-	$MarginContainer/Items/Slot0.visible = false
-	$MarginContainer/Items/Slot1.visible = false
+	$MarginContainer/MainContainer/Items/ItemLbl.visible = false
+	$MarginContainer/MainContainer/Items/Slot0.visible = false
+	$MarginContainer/MainContainer/Items/Slot1.visible = false
+	$MarginContainer/MainContainer/Skills/SkillLbl.visible = false
+	$MarginContainer/MainContainer/Skills/Slot0.visible = false
+	$MarginContainer/MainContainer/Skills/Slot1.visible = false
 	player=get_parent().get_parent().get_node("Player")
 
 func _process(_delta):
-	$MarginContainer/Items/ItemLbl.rect_position.y = 0
-	$MarginContainer/Items/Slot0.margin_left = 58
-	$MarginContainer/Items/Slot1.margin_left = 152
+	$MarginContainer/MainContainer/Items/ItemLbl.rect_position.y = 0
+	$MarginContainer/MainContainer/Skills/SkillLbl.rect_position.y = 0
+	
+	$MarginContainer/MainContainer/Items/Slot0.margin_left = 57
+	$MarginContainer/MainContainer/Items/Slot1.margin_left = 141
+	$MarginContainer/MainContainer/Skills.margin_top = 80
 
 func verifica_incrementa(item):
 	print("entrou"+item)
@@ -28,21 +37,21 @@ func verifica_incrementa(item):
 func adiciona_interface(item):
 	print("entrou2")
 	if slot == 0:
-		$MarginContainer/Items/ItemLbl.visible = true
-		$MarginContainer/Items/Slot0/Item0/Icon.texture = load("res://assets/img/gui/"+item+".png")
-		$MarginContainer/Items/Slot0/Lbl0.text = item
-		$MarginContainer/Items/Slot0.visible = true
+		$MarginContainer/MainContainer/Items/ItemLbl.visible = true
+		$MarginContainer/MainContainer/Items/Slot0/Item0/Icon.texture = load("res://assets/img/gui/"+item+".png")
+		$MarginContainer/MainContainer/Items/Slot0/Lbl0.text = item
+		$MarginContainer/MainContainer/Items/Slot0.visible = true
 		if item == "Lamp":
-			$MarginContainer/Items/Slot0/Item0/count.visible = false
+			$MarginContainer/MainContainer/Items/Slot0/Item0/count.visible = false
 		elif item == "Seed":
 			seedDisplay=0
 		slot += 1
 	else:
-		$MarginContainer/Items/Slot1/Item1/Icon.texture = load("res://assets/img/gui/"+item+".png")
-		$MarginContainer/Items/Slot1/Lbl1.text = item
-		$MarginContainer/Items/Slot1.visible = true
+		$MarginContainer/MainContainer/Items/Slot1/Item1/Icon.texture = load("res://assets/img/gui/"+item+".png")
+		$MarginContainer/MainContainer/Items/Slot1/Lbl1.text = item
+		$MarginContainer/MainContainer/Items/Slot1.visible = true
 		if item == "Lamp":
-			$MarginContainer/Items/Slot1/Item1/count.visible = false
+			$MarginContainer/MainContainer/Items/Slot1/Item1/count.visible = false
 		elif item == "Seed":
 			seedDisplay=1
 			pass
@@ -61,6 +70,6 @@ func _on_BlockBed_used_seed():
 
 func refreshSeeds():
 	if(seedDisplay==0):
-		$MarginContainer/Items/Slot0/Item0/count.set_text(str(countSeed))
+		$MarginContainer/MainContainer/Items/Slot0/Item0/count.set_text(str(countSeed))
 	elif (seedDisplay==1):
-		$MarginContainer/Items/Slot1/Item1/count.set_text(str(countSeed))
+		$MarginContainer/MainContainer/Items/Slot1/Item1/count.set_text(str(countSeed))
