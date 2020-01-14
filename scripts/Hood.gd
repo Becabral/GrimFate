bd4d3606fa4802d59e989e3b5e02fcb4f32449db
Miguel Duarte <miguelfelicianod@gmail.com>
2020-01-14 13:27:14 +0000

    Player changes sprite when picking up Hood
    Alteração ao Hood.gd que permite alterar o sprite do player assim que o Hood é colecionado

--- scripts/Hood.gd
+++ scripts/Hood.gd
@@ -15,12 +15,20 @@ func _ready():
 func _process(delta):
 	if(Input.is_action_pressed("use") && touch):
 			player.has_hood = true
+			changePlayer(player)
 			get_parent().get_node("Interface/GUI").adiciona_skill("Dash")
 			get_parent().get_node("FireEvent").show()
 			get_parent().get_node("FireEvent/StaticBody2D/CollisionShape2D").disabled=false
 			queue_free()
 #	pass
 
+func changePlayer(player):
+	if player.spritedir=="up":
+		player.get_node("RotPos/AnimatedSprite").play("Player_cloak_costas")
+	elif player.spritedir=="down":
+		player.get_node("RotPos/AnimatedSprite").play("Player_cloak_frente")
+	else:
+		player.get_node("RotPos/AnimatedSprite").play("Player_cloak_lateral")
 
 func _on_Hood_body_entered(body):
 	if body.get_name()=="Player":
