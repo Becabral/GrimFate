01c11a0aa5e483e7bd96335b4352ef4077966b98
Miguel Duarte <miguelfelicianod@gmail.com>
2020-01-15 17:53:14 +0000

    Script das narrativas para a pasta dos scripts

--- scripts/Narrative.gd
+++ scripts/Narrative.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
 export var text = ""
+var timer
+export var wait = 0.1
 
 func _ready():
 	var lab = get_node("Label")
 	lab.set_text(text)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
+	timer = Timer.new()
+	timer.set_wait_time(wait)
+	timer.connect("timeout",self,"_on_timer_timeout") 
+	add_child(timer)
+	timer.start()
+	
 
-
+func _on_timer_timeout():
+	$FadeIn.play("FadeIn", -1, 1)
+	timer.stop()
