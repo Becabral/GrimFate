cf60b4361d40f61fdd6110a7a43e5824cb31d691
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2020-01-16 16:05:31 +0000

    narrative tweak

--- scripts/Narrative.gd
+++ scripts/Narrative.gd
@@ -3,26 +3,20 @@ extends Node2D
 export var text = ""
 var timer
 export var wait = 0.1
+export var autostart = true 
+
 
 func _ready():
 	var lab = get_node("Label")
 	lab.set_text(text)
+	if autostart == true:
+		play_text()
 
-	
-	
-#	timer = Timer.new()
-#	timer.set_wait_time(wait)
-#	timer.connect("timeout",self,"_on_timer_timeout") 
-#	add_child(timer)
-#	timer.start()
 
 func play_text():
 	yield(get_tree().create_timer(wait), "timeout")
 	$FadeIn.play("FadeIn", -1, 0.7)
-#
-#func _on_timer_timeout():
-#	$FadeIn.play("FadeIn", -1, 1)
-#	timer.stop()
+
 
 func _on_OpeningScene_play_narrative():
 	play_text()
