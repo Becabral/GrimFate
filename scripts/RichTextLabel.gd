1bdec7573b537895f6ca5975e8a4d215dde8c4ce
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2020-01-17 18:36:16 +0000

    Dialgog choices

--- scripts/RichTextLabel.gd
+++ scripts/RichTextLabel.gd
@@ -1,13 +1,13 @@
 extends RichTextLabel
 
 var dialog
+var i = 0
 
-var die = ["Então Alfredo! Como vai o dia?" , "Cá vamos andando"]
-var live = ["Então Alfredo! Como vai o dia?" , "Cá vamos andando"]
-var death_bestie = ["Então Alfredo! Como vai o dia?" , "Cá vamos andando"]
+var live = ["Death: Greetings ungrateful mortal! I brought you here to show you how life is selfish and you weren't capable to pay attention. I could have put you in eternal peace but now I only want to extinguish you.", "choice" , "Death: I already made up my mind. I will not kill you. You can go back to life. Ask her to help you when you get too old and debilitated to amuse her"]
+var choice_live = ["Why am i here ?","Can i explain myself ?"]
 
 var page = 0
-
+var is_choice = false
 
 func _ready():
 	dialog = live
@@ -15,14 +15,54 @@ func _ready():
 	set_visible_characters(0)
 	set_process_input(true)
 	
-func _input(event):
+
+func _process(_delta):
 	if Input.is_action_just_pressed("use"):
+		check()
+	elif Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_up"):
+		if get_visible_characters() > get_total_character_count() and dialog[page] == "choice":
+			is_choice = false
+			i = i + 1
+			set_bbcode("Human: \n")
+			if i % 2 == 0:
+				push_color( Color( 1, 1, 1, 1 )) #white
+				append_bbcode(choice_live[0] + "\n")
+				pop()
+				push_color(Color(0,1,1))
+				append_bbcode(choice_live[1])
+				pop()
+			
+			else:
+				push_color(Color(0,1,1))
+				append_bbcode(choice_live[0] + "\n")
+				pop()
+				push_color( Color( 1, 1, 1, 1 )) #white
+				append_bbcode(choice_live[1])
+				pop()
+			
+		
+	
+func check():
+	if is_choice == false:
 		if get_visible_characters() > get_total_character_count():
 			if page < dialog.size() -1:
 				page += 1
-				set_bbcode(dialog[page])
-				set_visible_characters(0)
+				if dialog[page] == "choice":
+					is_choice = true
+					set_bbcode("Human: \n")
+					push_color(Color(0,1,1))
+					append_bbcode(choice_live[0] + "\n")
+					pop()
+					push_color( Color( 1, 1, 1, 1 )) #white
+					append_bbcode(choice_live[1])
+					pop()
+					set_visible_characters(0)
+					
+				else:
+					set_bbcode(dialog[page])
+					set_visible_characters(0)
 
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters() + 1)
+
