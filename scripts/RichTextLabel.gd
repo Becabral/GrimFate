424f884edd1aa6d75313f82e1ab12f8caed15c05
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2020-01-17 21:31:31 +0000

    ritchtext label v3

--- scripts/RichTextLabel.gd
+++ scripts/RichTextLabel.gd
@@ -2,18 +2,19 @@ extends RichTextLabel
 
 var dialog
 var choice
-var i = 0
+var i = -1
 onready var global = get_node("/root/SceneChanger")
 
-var live = ["Death: \nGreetings ungrateful mortal! \nI brought you here to show you how life is selfish and you weren't capable to pay attention. \nI could have put you in eternal peace but now I only want to extinguish you.", "choice" , "Death: \nI already made up my mind... I will not kill you... You can go back to life. \nAsk her to help you when you get too old and debilitated to amuse her"]
+var live = ["Death: \nGreetings ungrateful mortal! \nI brought you here to show you how life is selfish and you weren't capable to pay attention. \nI could have put you in eternal peace but now I only want to extinguish you.", "choice" , "Death: \nI already made up my mind... I will not kill you... You can go back to life. \nAsk her to help you when you get too old and debilitated to amuse her", "end1"]
 var choice_live = ["Why am i here ?","Can i explain myself ?"]
 
 var die
-var i_death = ["Greetings my dear mortal!", "I’ve seen you have what it takes to understand what i do." , "I would like to give you the full tour on life but it appears we all have deadlines." , "You see this isn’t a game where you can manipulate your world with rules.", "I brought you here to show you how life is cruel to you and you seem to understand \ntherefore i will make you and offer my dear mortal","Do you want to be like me ? ", "choice"]
+var i_death = ["Greetings my dear mortal!", "I’ve seen you have what it takes to understand what i do." , "I would like to give you the full tour on life but it appears we all have deadlines." , "You see this isn’t a game where you can manipulate your world with rules.", "I brought you here to show you how life is cruel to you and you seem to understand \ntherefore i will make you and offer my dear mortal","Do you want to be like me ? ", "choice" , "end2" ]
 var choice_i_death = ["Yes","No"]
 
 var page = 0
 var is_choice = false
+var choice_made = false
 
 func _ready():
 	"""if global.lamp and global.alternative_path:
@@ -40,6 +41,7 @@ func _process(_delta):
 			i = i + 1
 			set_bbcode("Human: \n")
 			if i % 2 == 0:
+				choice_made = false
 				push_color( Color( 1, 1, 1, 1 )) #white
 				append_bbcode(choice[0] + "\n")
 				pop()
@@ -48,6 +50,7 @@ func _process(_delta):
 				pop()
 			
 			else:
+				choice_made = true
 				push_color(Color(0,1,1))
 				append_bbcode(choice[0] + "\n")
 				pop()
@@ -73,6 +76,13 @@ func check():
 					pop()
 					set_visible_characters(0)
 					
+				elif dialog[page] == "end1":
+					print("go to live scene")
+				elif dialog[page] == "end2":
+					if choice_made:
+						print("go to is_death scene")
+					else:
+						print("go to is_death scene2")
 				else:
 					set_bbcode(dialog[page])
 					set_visible_characters(0)
