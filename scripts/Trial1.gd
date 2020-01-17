8bcf9d3a391671794c1d4e53dade9e35b9e5771f
Miguel Duarte <miguelfelicianod@gmail.com>
2020-01-17 19:54:55 +0000

    Ice Door now "Melts"

--- scripts/Trial1.gd
+++ scripts/Trial1.gd
@@ -50,3 +50,10 @@ func _on_SmoothOn_body_entered(body):
 			
 			outside=false
 	pass # Replace with function body.
+
+
+func _on_SnowWallArea_body_entered(body):
+	if body.get_name()=="Player":
+		if body.on_fire:
+			$SnowWall/AnimSnowWall.play("Melt")
+	pass # Replace with function body.
