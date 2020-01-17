71a95151c419b72e581fe6e0b82e800f471d5530
Becabral <Bebrasilcabral@gmail.com>
2020-01-17 22:42:57 +0000

    Aprimoramento dos assets do PauseGui

--- scripts/PausedGUI.gd
+++ scripts/PausedGUI.gd
@@ -5,25 +5,25 @@ func _ready():
 	$Container.hide()
 	$Fundo.hide()
 	$Container/VBoxContainer/HBoxContainer/ResumeContainer.margin_left = 0
-	$Container/VBoxContainer/HBoxContainer/ResumeContainer.margin_top = -7
+	$Container/VBoxContainer/HBoxContainer/ResumeContainer.margin_top = 0
 	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_bottom = 0
 	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_left = 90
-	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_top = -7
+	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_top = 0
 	
-	$Container/VBoxContainer/HBoxContainer/ResumeContainer/LblResume.margin_top = 38
-	$Container/VBoxContainer/HBoxContainer/QuitContainer/LblQuit.margin_top = 38
+	$Container/VBoxContainer/HBoxContainer/ResumeContainer/LblResume.margin_top = 45
+	$Container/VBoxContainer/HBoxContainer/QuitContainer/LblQuit.margin_top = 45
 	
 	
 	
 func _process(_delta):
 	$Container/VBoxContainer/HBoxContainer/ResumeContainer.margin_left = 0
-	$Container/VBoxContainer/HBoxContainer/ResumeContainer.margin_top = -7
+	$Container/VBoxContainer/HBoxContainer/ResumeContainer.margin_top = 0
 	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_bottom = 0
 	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_left = 90
-	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_top = -7
+	$Container/VBoxContainer/HBoxContainer/QuitContainer.margin_top = 0
 	
-	$Container/VBoxContainer/HBoxContainer/ResumeContainer/LblResume.margin_top = 38
-	$Container/VBoxContainer/HBoxContainer/QuitContainer/LblQuit.margin_top = 38
+	$Container/VBoxContainer/HBoxContainer/ResumeContainer/LblResume.margin_top = 45
+	$Container/VBoxContainer/HBoxContainer/QuitContainer/LblQuit.margin_top = 45
 
 
 func _on_BtnResume_pressed():
