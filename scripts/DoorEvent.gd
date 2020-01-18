205b1427813ec0cf1cf5e399984c8e6249f75fc0
Miguel Duarte <miguelfelicianod@gmail.com>
2020-01-18 11:35:29 +0000

    Trial 1 - Tried to make torch closing door more obvious

--- scripts/DoorEvent.gd
+++ scripts/DoorEvent.gd
@@ -3,7 +3,8 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var playercamera
+var torchunlit = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,6 +16,16 @@ func _process(_delta):
 			$Door.queue_free()
 	
 	if has_node("BigTorch"):
+		if !$BigTorch.lit and !torchunlit :
+			torchunlit=true
+			playercamera=get_parent().get_node("Player/Camera2D")
+			playercamera.limit_top=-1800
+			playercamera.limit_bottom=-740
+			playercamera.limit_left=-1200
+			yield(get_tree().create_timer(2), "timeout")
+			playercamera.limit_top=-1000000
+			playercamera.limit_bottom=1000000
+			playercamera.limit_left=-1000000
 		if $BigTorch.blue and $BigTorch.lit and has_node("Door"):
 			$Door.queue_free()
 #	pass
