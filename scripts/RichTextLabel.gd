56f47100aa14fcea4bf8ba94ebd73da417756c64
ivoamaro <33934913+ivoamaro@users.noreply.github.com>
2020-01-19 18:44:23 +0000

    new narrative added

--- scripts/RichTextLabel.gd
+++ scripts/RichTextLabel.gd
@@ -6,13 +6,13 @@ var i = -1
 var a = 0 
 onready var global = get_node("/root/SceneChanger")
 
-var live = ["Death:\nGreetings my dear mortal.\nI’ve seen you have what it takes to understand what i do.","Death:\nI would like to give you the full tour but it appears we all have deadlines.","Death:\nYou seem to not understanding at all the importance of my existence, therefore, i will make you go back to your existence knowing i will not embrace you when you cease.", "choice", "live"]
-var choice_live = ["Can i make you change your mind?","I prefer that than being like you"]
+var live = ["Death:\nGreetings, dear mortal. \nYour round of trials is now at its end.","Death:\nI would have liked to give you the full tour but it appears we all have deadlines.","Death:\nIt seems you elect to ignore the meaning behind my existence. You shall have your way.","In time, I will return you to your existence.","But know this. \nNext time you perish, I shall not be there to embrace you.", "choice", "live"]
+var choice_live = ["Your words mean nothing to me. Return me at once.","Is there a way for you to reconsider?"]
 
-var die = ["Death:\nGreetings my dear mortal.\nI’ve seen you have what it takes to understand what i do.","Death:\nI would like to give you the full tour but it appears we all have deadlines.","Death:\nI noticed you lack of standards and will do whatever i want just to plase me.","Death:\nI thought you would understand. If you don’t understand what i do, the only thing that remains for you is living forever without being alive at all. Welcome to limbo","choice", "die"]
+var die = ["Death:\nGreetings, dear mortal.\nYour round of trials is now at its end.","Death:\nI would have liked to give you the full tour but it appears we all have deadlines.","Death:\nYou have struggled. The meaning of all this still eludes you.","die_dual","choice", "die"]
 var choice_die = ["...", "..."]
 
-var i_death = ["Death: \nGreetings my dear mortal. \nI’ve seen you have what it takes to understand what i do.", "Death:\nI would like to give you the full tour but it appears we all have deadlines.","Death:\nYou seem to understand the importance of my existence, therefore, i will make you a proposal you can’t deny.", "choice", "i_death" ]
+var i_death = ["Death:\nGreetings, dear mortal.\nYour round of trials is now at its end.","Death:\nI would have liked to give you the full tour but it appears we all have deadlines.","Death:\nI’m pleased to see that you seem to understand the importance of my existence. Therefore, I shall make you a proposal.\nOne which you cannot deny.", "choice", "i_death" ]
 var choice_i_death = ["Yes","No"]
 
 var page = 0
@@ -62,7 +62,7 @@ func _process(_delta):
 			is_choice = false
 			i = i + 1
 			if dialog == i_death:
-				set_bbcode("Death: Do you want to do what i do?\nHuman: \n")
+				set_bbcode("Since you have proven to have the makings, will you take on the mantle of Death?\nHuman: \n")
 			else:
 				set_bbcode("Human: \n")
 			if i % 2 == 0:
@@ -93,7 +93,7 @@ func check():
 				if dialog[page] == "choice":
 					is_choice = true
 					if dialog == i_death:
-						set_bbcode("Death: Do you want to do what i do?\nHuman: \n")
+						set_bbcode("Since you have proven to have the makings, will you take on the mantle of Death?\nHuman: \n")
 					else:
 						set_bbcode("Human: \n")
 						
@@ -107,24 +107,32 @@ func check():
 					
 				elif dialog[page] == "live":
 					if choice_made or !choice_made:
-						set_bbcode("Farewell ignorant human! i hope you’ll enjoy the time you have left")
+						set_bbcode("Farewell ignorant being! Enjoy the time you have left.")
 						final_reached = true
 				elif dialog[page] == "i_death":
 					if choice_made:
-						set_bbcode("Death:\nVery well! Welcome my dear friend.\nI knew you would understand me. Your finest moments are yet to come.")
+						set_bbcode("Death:\nVery well! Welcome my dear friend.\nYour finest moments are yet to come.")
 						set_visible_characters(0)
 						final_reached = true	
 					else:
-						set_bbcode("Death:\nNo?! Why ?!\nI thought you would understand.\nIf no one can understand me i will leave you. Good luck trying to balance life without me. ")
+						set_bbcode("Death:\n...I see. You deny my role even though you understand it. Clearly, you must find no place for it to continue. Very well then. I shall take my leave and withdraw from my duties. Remain here and lay witness to how valuable your deathless world really is.")
 						set_visible_characters(0)
 						final_reached = true	
 				elif dialog[page] == "die":
 					if choice_made or !choice_made:
 						set_bbcode("Farewell ignorant human!")
 						final_reached = true
+				elif dialog[page] == "die_dual":
+					if global.lamp == true:
+						set_bbcode("Death:\nYou kept hold of your essence, yet were still compelled to ford through my trials.\nOne who knows not the road they travel shall remain lost in limbo. Living forever without being alive at all.")
+						set_visible_characters(0)
+					elif global.alternative_path == true:
+						set_bbcode("Death:\nYou strayed from your path yet were unable to keep your essence.\nOne who knows not the road they travel shall remain lost in limbo. Living forever without being alive at all.")
+						set_visible_characters(0)
 				else:
 					set_bbcode(dialog[page])
-					set_visible_characters(0)		
+					set_visible_characters(0)
+
 					
 					
 
@@ -134,7 +142,6 @@ func _on_Timer_timeout():
 		set_visible_characters(get_visible_characters() + 1)
 		if get_visible_characters() > get_total_character_count():
 			a = a + 1
-			count_end += 1
 	else:
 		set_visible_characters(get_visible_characters() + get_total_character_count())
 		
